385f11f7cabdbff1cf1d5b97cd889a5ba42a812f
pribob <roland.rehfeld@hotmail.com>
2023-03-09 23:01:04 +0100

    Done.All done.

--- script.js
+++ script.js
@@ -1,61 +1,55 @@
 
-$(function() {
-    $("#add").on("click", addItem);
-    });  
+$(function() {$("#add").on("click", addItem);});  
+$(function() {$("#hide").on("click", toggleList);});  
+$(function() {$("#item").keypress(function(event){
+  var keycode = event.key;
+  if(keycode == 'Enter'){
+    addItem();
+  }
+});});  
 $(()=>{$("#listStart").on("click", handleDeleteItem)});
+
+function toggleList(){
+  $("#sortable").toggle();
+  if ($("#hide").html() == "hide")
+  {
+    $("#hide").html("show");
+  }
+  else{
+    $("#hide").html("hide");
+  }
+}
+
 
 function addItem() {
         console.log("clicked");
+        if ($("#item").val()!="")
+        {
         e = $("#item").val();
-        console.log(e);
-        $("ol").append("<li>" + e + "</li>");
+        console.log(e);       
+        $("ol").append("<li>" + e + "</li>").css("display", "none").show("slow ");
         // instead of class add eventlistener?
         $("li:contains('" + e + "')").click(
             handleDeleteItem);
         $("#item").val("");
         console.log("element added");
+        }
+        else {
+          console.log("Empty input!");
+        }
     }
 
 function handleDeleteItem () {
     console.log(this);
-    $(this).remove();
+    let conf = confirm("Sure you wanna delete this element?");
+    if (conf)
+    {
+
+      $(this).remove();
+    }
 }
 
-function enableDragSort(listClass) {
-    const sortableLists = document.getElementsByClassName(listClass);
-    Array.prototype.map.call(sortableLists, (list) => {enableDragList(list)});
-  }
-  
-  function enableDragList(list) {
-    Array.prototype.map.call(list.children, (item) => {enableDragItem(item)});
-  }
-  
-  function enableDragItem(item) {
-    item.setAttribute('draggable', true)
-    item.ondrag = handleDrag;
-    item.ondragend = handleDrop;
-  }
-  
-  function handleDrag(item) {
-    const selectedItem = item.target,
-          list = selectedItem.parentNode,
-          x = event.clientX,
-          y = event.clientY;
-    
-    selectedItem.classList.add('drag-sort-active');
-    let swapItem = document.elementFromPoint(x, y) === null ? selectedItem : document.elementFromPoint(x, y);
-    
-    if (list === swapItem.parentNode) {
-      swapItem = swapItem !== selectedItem.nextSibling ? swapItem : swapItem.nextSibling;
-      list.insertBefore(selectedItem, swapItem);
-    }
-  }
-  
-  function handleDrop(item) {
-    item.target.classList.remove('drag-sort-active');
-  }
-  
-  (()=> {enableDragSort('drag-sort-enable')})();
+
 
     
     
